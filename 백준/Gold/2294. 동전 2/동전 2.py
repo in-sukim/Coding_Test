da98fdf08d68ce4edfa978fcf820ec6abba6e165
Insu Kim <rladlstn494@naver.com>
2023-12-04 16:26:17 +0900

    [Gold V] Title: 동전 2, Time: 200 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,16 +1,24 @@
-n,k = map(int,input().split())
-tokens = [int(input()) for _ in range(n)]
+[n,k] = list(map(int,input().split())) # k=15
 
+coin_set = []
+for i in range(n):
+    coin_set.append(int(input()))
+coin_set = list(set(coin_set)) #1,5,12
 
-dp = [99999 for _ in range(k+1)]
-dp[0] = 0
+cnt = [-1]*(k+1)
+cnt[0] = 0 # cnt = [0,-1,-1,-1,...]
+for i in range(1,k+1):
+    if i>=min(coin_set):
+        min_cnt = [] # coin_set의 각 동전 사용하는 경우
+        for coin in coin_set:
+            if i>=coin:
+                min_cnt.append(cnt[i-coin]) 
+        min_cnt = sorted(list(set(min_cnt)))
+        if min_cnt[-1]==-1: # 모든 경우 -1
+            continue
+        elif min_cnt[0]==-1:
+            cnt[i] = 1 + min_cnt[1] # -1/-1 아닌 경우 모두 존재
+        else:
+            cnt[i] = 1 + min_cnt[0]
 
-for token in tokens:
-  for now in range(1,k+1):
-    if now - token >= 0:
-      dp[now] = min(dp[now], dp[now-token] + 1)
-
-if dp[k] == 99999:
-  print(-1)
-else:
-  print(dp[k])
+print(cnt[-1])
