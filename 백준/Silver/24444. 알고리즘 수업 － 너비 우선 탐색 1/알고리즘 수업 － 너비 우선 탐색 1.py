979b9216343cc0af09c4f65779e27a8c5ed5e897
Insu Kim <rladlstn494@naver.com>
2024-07-03 22:34:59 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 544 ms, Memory: 58624 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -1,35 +1,36 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-n,m,r = map(int,input().split())
-visited = [0] * (n+1)
+N, M, R = map(int, input().split())
 
-graph = [[] for _ in range(n+1)]
+graph = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+count = 1
 
-order = 1
-def bfs(graph, start, visited):
-  global order
-  q = deque([start])
-  visited[start] = order
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-  while q:
-    v = q.popleft()
-    for i in graph[v]:
-      if not visited[i]:
-        q.append(i)
-        order += 1
-        visited[i] = order
+for i in range(1, N+1):
+    graph[i].sort()
+
+def bfs(start):
+    queue = deque([start])
+    global count #방문 순서 업데이트
+    visited[start] = count
+    count += 1
+    while queue:
+        a = queue.popleft()
         
-for _ in range(m):
-  a,b = map(int,input().split())
-  graph[a].append(b)
-  graph[b].append(a)
+        for i in graph[a]:
+            if visited[i] == 0:
+                queue.append(i)
+                visited[i] = count
+                count += 1
 
+bfs(R)
 
-for i in range(n+1):
-  graph[i].sort()
-
-        
-bfs(graph, r, visited)
-
-for i in range(1, n+1):
-  print(visited[i])
+for i in range(1, N+1):
+    print(visited[i])
