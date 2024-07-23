8d09322d09daaf57494600f155f880ee250c0e81
Insu Kim <rladlstn494@naver.com>
2024-07-24 08:47:16 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,20 +1,41 @@
+# import math
+# from collections import deque
+# def solution(progresses, speeds):
+#     days = deque()
+#     answer = []
+#     for progress, speed in zip(progresses, speeds):
+#         day = (100 - progress) / speed
+#         days.append(math.ceil(day))
+        
+#     while days:
+#         c = days.popleft()
+#         cnt = 1
+        
+#         while days and c >= days[0]:
+#             days.popleft()
+#             cnt += 1
+#         answer.append(cnt)
+#     return answer
+    
+
 import math
-from collections import deque
+
 def solution(progresses, speeds):
-    days = deque()
+    days = []
     answer = []
     for progress, speed in zip(progresses, speeds):
         day = (100 - progress) / speed
         days.append(math.ceil(day))
-        
-    while days:
-        c = days.popleft()
-        cnt = 1
-        
-        while days and c >= days[0]:
-            days.popleft()
-            cnt += 1
-        answer.append(cnt)
+
+    cnt = 0
+    before_day = days[0]
+    for i in range(len(days)):
+        if before_day < days[i]:
+            answer.append(cnt)
+            before_day = days[i]
+            cnt = 0
+        cnt += 1
+    answer.append(cnt)
+
+
     return answer
-    
-        
