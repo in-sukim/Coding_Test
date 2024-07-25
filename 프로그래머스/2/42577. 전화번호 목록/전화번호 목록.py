24ff4dd1c597c9683629dec26292f0d6d7876f8e
Insu Kim <rladlstn494@naver.com>
2024-07-25 15:16:02 +0900

    [level 2] Title: 전화번호 목록, Time: 470.09 ms, Memory: 54.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,17 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
+def solution(phone_book):
+    phone_dic = defaultdict(int)
+    for phone in phone_book:
+        phone_dic[phone] = hash(phone)
+
+    phone_book.sort(key =  lambda x: len(x), reverse = True)
+    for phone in phone_book:
+        temp = ''
+        for num in phone:
+            temp += num
+            if (temp in phone_dic) and (temp != phone):
+                return False
+    return True
