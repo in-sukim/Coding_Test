d066c32ea417bd02bd7e4401fefbbeb350b3f06a
Insu Kim <rladlstn494@naver.com>
2024-07-26 14:57:12 +0900

    [level 2] Title: 모음 사전, Time: 6.40 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,12 +1,18 @@
-from itertools import product
-def solution(word):
-    alpha = ["A","E","I","O","U"]
-    all_alpha = []
-    for i in range(1, len(alpha)+1):
-        value = [''.join(i) for i in list(product(alpha, repeat = i))]
-        all_alpha.extend(value)
-    all_alpha.sort()
 
-    for idx, i in enumerate(all_alpha):
-        if i == word:
-            return idx + 1
+
+def dfs(x, dictionary, alpha):
+    dictionary.append(x)
+    for i in alpha : 
+        if len(x) != len(alpha) :
+            dfs(x+i, dictionary, alpha)
+        else :
+            x[:-1]
+            
+
+def solution(word):
+    alpha = ['A','E','I','O','U']
+    dictionary = []
+    for i in range(len(alpha)) :
+        dfs(alpha[i], dictionary, alpha)
+    
+    return dictionary.index(word) + 1
