293c3d8d8b948052fb0302aa66de09690a75961c
Insu Kim <rladlstn494@naver.com>
2024-08-09 23:42:42 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 194.73 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -0,0 +1,22 @@
+import math
+def solution(arrayA, arrayB):
+    a = arrayA[0]
+    b = arrayB[0]
+    
+    for i in range(len(arrayA)):
+        a = math.gcd(a, arrayA[i])
+        b = math.gcd(b, arrayB[i])
+    print(a,b)
+    
+    cul = 1
+    young = 1
+    
+    for i in range(len(arrayA)):
+        if arrayB[i] % a == 0:
+            cul = 0
+        if arrayA[i] % b == 0:
+            young = 0
+    if cul == 0 and young == 0:
+        return cul
+    else:
+        return max(a,b)
