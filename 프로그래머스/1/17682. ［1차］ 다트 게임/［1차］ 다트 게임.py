1f8ed4272a6bf68393780e44cca368c645565489
Insu Kim <rladlstn494@naver.com>
2024-10-27 16:39:14 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,28 +1,25 @@
 def solution(dartResult):
-    result = []
-    score = 0
-    dartResult = dartResult.replace('10','t')
+    dartResult = dartResult.replace('10','n')
+    SDT = {
+        'S': 1,
+        'D': 2,
+        'T': 3
+    }
+    score = []
+    
     for i in dartResult:
-        if i.isnumeric():
-            score += int(i)
-            continue
-        elif i == 't':
-            score += 10
-            continue
-        elif i == 'S':
-            result.append(score)
-        elif i == 'D':
-            result.append(score ** 2)
-        elif i == 'T':
-            result.append(score ** 3)
-        elif i == '*':
-            if len(result) > 1:
-                result[-1] *= 2
-                result[-2] *= 2
+        if i.isdigit() or i == 'n':
+            if i == 'n':
+                score.append(10)
             else:
-                result[-1] *= 2
+                score.append(int(i))
+        elif i in SDT:
+            cur_score = score.pop()
+            score.append(cur_score ** SDT[i])
+        elif i == '*':
+            score[-1] *= 2
+            if len(score) > 1:
+                score[-2] *= 2
         elif i == '#':
-            result[-1] *= -1
-        score = 0
-    print(result)
-    return sum(result)
+            score[-1] *= -1
+    return sum(score)
