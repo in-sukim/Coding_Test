57fc0b6632c335b23415223f29e1aa4324da234f
Insu Kim <rladlstn494@naver.com>
2024-10-29 10:47:02 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -11,15 +11,8 @@ def solution(lottos, win_nums):
         5 : 2,
         4 : 3,
         3 : 4,
-        2 : 5
+        2 : 5,
+        1 : 6,
+        0 : 6
     }
-    if max_num in rank_dic:
-        max_num = rank_dic[max_num]
-    else:
-        max_num = 6
-        
-    if min_num in rank_dic:
-        min_num = rank_dic[min_num]
-    else:
-        min_num = 6
-    return [max_num, min_num]
+    return [rank_dic[max_num], rank_dic[min_num]]
