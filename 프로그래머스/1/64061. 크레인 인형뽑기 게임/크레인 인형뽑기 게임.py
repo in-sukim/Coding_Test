d809974ac0cc8fa8f8b6cd7beede3500c63523f6
Insu Kim <rladlstn494@naver.com>
2024-10-31 15:33:57 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.20 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,32 +1,15 @@
 def solution(board, moves):
-    col = len(board[0])
-    new_board = []
-    picked = []
-    for c in range(col):
-        temp = []
-        temp_pick = []
-        for i in board:
-            flag = True
-            temp.append(i[c])
-            if i[c] == 0:
-                flag = False
-            temp_pick.append(flag)
-        new_board.append(temp[::-1])
-        picked.append(temp_pick[::-1])
-    
     cnt = 0
     bucket = []
     for move in moves:
-        possible_pick = [idx for idx, i in enumerate(picked[move-1]) if i]
-        if not len(possible_pick):
-            break
-        else:
-            pick_idx = max(possible_pick)
-        pick = new_board[move-1][pick_idx]
-        if bucket and bucket[-1] == pick:
-            bucket.pop(-1)
-            cnt += 2
-        else:
-            bucket.append(pick)
-        picked[move-1][pick_idx] = False
+        for i in range(len(board)):
+            pick = board[i][move-1]
+            if pick:
+                if bucket and bucket[-1] == pick:
+                    bucket.pop(-1)
+                    cnt += 2
+                else:
+                    bucket.append(pick)
+                board[i][move-1] = 0
+                break
     return cnt
