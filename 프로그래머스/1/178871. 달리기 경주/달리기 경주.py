7dfd20ce849f50cc82219effb2143e6b45f62cf1
Insu Kim <rladlstn494@naver.com>
2024-11-01 17:14:05 +0900

    [level 1] Title: 달리기 경주, Time: 464.23 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,15 +1,11 @@
 def solution(players, callings):
     player_order = dict()
-    for idx, player in enumerate(players):
-        player_order[player] = idx
+    player_order = {key: idx for idx, key in enumerate(players)}
         
     for call in callings:
         call_idx = player_order[call]
         ahead_idx = call_idx - 1
         player_order[call], player_order[players[ahead_idx]] = ahead_idx, call_idx
         players[ahead_idx], players[call_idx] = players[call_idx], players[ahead_idx]
-    player_order = [i[0] for i in sorted(player_order.items(), key = lambda x: x[1])]
-    return player_order
-        
+    return players
     
-        
