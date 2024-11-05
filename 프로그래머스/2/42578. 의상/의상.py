800884d42a09c6636e8a4f5b03b980667cfd53ff
Insu Kim <rladlstn494@naver.com>
2024-11-05 17:28:32 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,11 @@
 from collections import defaultdict
 def solution(clothes):
     clothes_dic = defaultdict(list)
-
-    for name, type in clothes:
-        clothes_dic[type].append(name)
-
-    answer = 1
-    clothes_list = list(map(len, clothes_dic.values()))
-    for i in clothes_list:
-        answer *= i+1
-    return answer-1
+    for name, category in clothes:
+        clothes_dic[category].append(name)
+    
+    possible =list(map(lambda x: len(x)+1, clothes_dic.values()))
+    result = 1
+    for p in possible:
+        result *= p
+    return result - 1
