e2f5867a34e8db25ca787fcccb10368294e14b0e
Insu Kim <rladlstn494@naver.com>
2024-11-12 22:55:46 +0900

    [level 2] Title: 땅따먹기, Time: 255.43 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,7 +1,14 @@
 def solution(land):
-
-    for i in range(1,len(land)):
-        for j in range(len(land[0])):
-            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
-
-    return max(land[len(land)-1])
+    N = len(land)
+    
+    dp = [[0] * 4 for _ in range(N)]
+    dp[0] = land[0][:]
+    
+    for i in range(1, N):
+        for j in range(4):
+            dp[i][j] = land[i][j] + max(
+                dp[i-1][(j+1)%4],
+                dp[i-1][(j+2)%4],
+                dp[i-1][(j+3)%4]
+            )
+    return max(dp[N-1])
