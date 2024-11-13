4dab65c54e23ad65c89e6f9a3e038943472bd226
Insu Kim <rladlstn494@naver.com>
2024-11-13 15:08:37 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 5.61 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,14 +1,28 @@
-import re
 def solution(files):
-    answer = []
-    file = []
-
-    for idx, i in enumerate(files):
-        number = re.findall("\d+", i)
-        head = i[:i.index(number[0])].lower()
-        file.append([head, int(number[0]), idx])
-
-    file.sort(key = lambda x: [x[0], x[1], x[2]])
-    for i in file:
-        answer.append(files[i[-1]])
-    return answer
+    new_files = []
+    for f_idx, file in enumerate(files):
+        before = file[0].isdecimal()
+        standard = []
+        num_start = -1
+        for idx, i in enumerate(file):
+            current = i.isdecimal()
+            if current != before:
+                standard.append(idx)
+                if current:
+                    num_start = idx
+            before = current
+            
+            if num_start != -1 and current and idx - num_start >= 5:
+                if len(standard) % 2 == 1:
+                    standard.append(idx)
+                    break
+        if not standard:
+            continue
+            
+        head = file[:standard[0]].lower()
+        number_end = min(standard[0] + 5, standard[1] if len(standard) > 1 else len(file))
+        number = int(file[standard[0]:number_end])
+        new_files.append([head, number, f_idx])
+    new_files.sort(key = lambda x: (x[0], x[1], x[2]))
+    result = [files[i[-1]] for i in new_files]
+    return result
