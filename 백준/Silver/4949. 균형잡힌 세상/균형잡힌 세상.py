703b17c92cfd885bfb53c1b6fb08f06a45f81509
Insu Kim <rladlstn494@naver.com>
2025-01-16 00:20:35 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,34 +1,36 @@
 import sys
-import re
 
-def brakets(lines):
-  stack = []
-  for i in lines:
-    if i == '(' or i == '[':
-      stack.append(i)
-    elif i == ")":
-      if not stack:
-        return 'no'
-      elif stack[-1] == '(':
-        stack.pop()
-      else:
-        return 'no'
-    elif i == "]":
-      if not stack:
-        return 'no'
-      elif stack[-1] == '[':
-        stack.pop()
-      else:
-        return 'no'
-  if not stack:
-    return 'yes'
-  else:
-    return 'no'
+
+def brackets(lines):
+    stack = []
+    for i in lines:
+        if i == "(" or i == "[":
+            stack.append(i)
+        elif i == ")":
+            if not stack:
+                return "no"
+            elif stack[-1] == "(":
+                stack.pop()
+            else:
+                return "no"
+        elif i == "]":
+            if not stack:
+                return "no"
+            elif stack[-1] == "[":
+                stack.pop()
+            else:
+                return "no"
+    if not stack:
+        return "yes"
+    else:
+        return "no"
+
 
 answer = []
 while True:
-  line = sys.stdin.readline()
-  if line == '.\n':
-    break
-  print(brakets(line))
-    
+    line = sys.stdin.readline()
+    if line == ".\n":
+        break
+    answer.append(brackets(line))
+
+print("\n".join(answer))
