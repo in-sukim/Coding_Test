26deafea4d0f4f2f475c8856e050d66d8025727c
Insu Kim <rladlstn494@naver.com>
2025-01-20 13:35:47 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 51888 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,15 +1,7 @@
 import sys
 from collections import deque
-N = int(sys.stdin.readline())
-q = deque()
-for i in range(1, N+1):
-  q.append(i)
-
-idx =0
-while len(q) > 1:
-  if idx % 2 == 0:
-    q.popleft()
-  else:
-    q.append(q.popleft())
-  idx += 1
-print(q[0])
+n = deque(range(1, int(sys.stdin.readline())+1,))
+while len(n) != 1:
+    n.popleft()
+    n.rotate(-1)
+print(n[0])
