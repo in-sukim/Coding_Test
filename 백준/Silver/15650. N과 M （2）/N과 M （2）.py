9547c26ee04af125e785d9b85b5a1d293c177ca6
Insu Kim <rladlstn494@naver.com>
2025-01-22 12:57:21 +0900

    [Silver III] Title: N과 M (2), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,14 @@
+import sys
+
+n,m = map(int, sys.stdin.readline().split())
+result = []
+
+def back(start) :
+  if len(result) == m:
+    print(' '.join(map(str, result)))
+  for i in range(start, n+1):
+   if i not in result:
+     result.append(i)
+     back(i+1)
+     result.pop()
+back(1)
