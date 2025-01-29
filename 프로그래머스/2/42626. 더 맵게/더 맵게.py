70de4e5646a975f53f68b92fa90897c57540be55
Insu Kim <rladlstn494@naver.com>
2025-01-29 12:17:32 +0900

    [level 2] Title: 더 맵게, Time: 1672.64 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,23 +1,20 @@
-import heapq
-def solution(scoville, k):
-    heap = []
-    for s in scoville:
-        heapq.heappush(heap, s)
-    
-    cnt = 0
-    while len(heap) >= 2:
-        if heap[0] >= k:
-            return cnt
-        
-        min_value = heapq.heappop(heap)
-        min_next_value = heapq.heappop(heap)
-        new_scoville = min_value + (min_next_value * 2)
-        heapq.heappush(heap, new_scoville)
-        cnt += 1
-    
-    if heap and heap[0] >= k:
-        return cnt
-    return -1
+import heapq as hq
 
-        
+def solution(scoville, K):
+    hq.heapify(scoville)
+    answer = 0
     
+    while len(scoville) >= 2:
+        first = hq.heappop(scoville)
+        if first >= K:
+            hq.heappush(scoville, first)  # 다시 넣어주고
+            break
+            
+        second = hq.heappop(scoville)
+        hq.heappush(scoville, first + second*2)
+        answer += 1
+    
+    # 모든 값이 K 이상인지 확인
+    if scoville and scoville[0] >= K:
+        return answer
+    return -1
